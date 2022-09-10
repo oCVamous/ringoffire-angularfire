74098789c76589bf984f5a9b71df2df89562dcb2
Patrick Sterz <82339311+oCVamous@users.noreply.github.com>
2022-09-11 01:23:20 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+import { GameComponent } from './game/game.component';
+import { PlayerComponent } from './player/player.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from './dialog-add-player/dialog-add-player.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { GameInfoComponent } from './game-info/game-info.component';
+import { MatCardModule } from '@angular/material/card';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StartScreenComponent,
+    GameComponent,
+    PlayerComponent,
+    DialogAddPlayerComponent,
+    GameInfoComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule, 
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    FormsModule,
+    MatCardModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/game/game.component.ts
@@ -0,0 +1,118 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../models/game';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { MatDialog, MatDialogRef} from '@angular/material/dialog';
+import { Firestore, collectionData, collection, setDoc, doc, addDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+
+
+
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game!: Game;
+  games$: Observable<any[]> | undefined;
+  games : Array<any> | undefined;
+  coll: any;
+
+  // private firestore: getFirestore,
+  constructor(private firestore: Firestore, public dialog: MatDialog) {
+    
+    // this.games = firestore.collection('games').valueChanges();
+
+
+
+    // _______________________________________
+    // const coll2 = collection(firestore, 'games');
+    // this.todos$ = collectionData(coll);
+
+    // this.todos$.subscribe( newTodos => {
+    //   console.log('neue Todos sind:', newTodos);
+    //   this.todos = newTodos;
+    //   alert('New Todo UPDATE');
+    // });   
+   }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent); 
+
+    dialogRef.afterClosed().subscribe((name : string ) => {
+      if(name && name.length > 0) {
+        this.game.players.push(name);
+      }
+    });
+  }
+
+  // openDialog(): void {
+  //   const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+   
+  //    dialogRef.afterClosed().subscribe((name: string => {
+  //     if(name && name.length > 0) {
+  //        this.game.players.push(name);
+  //      }
+  //    });
+  //  }
+
+  ngOnInit(): void {
+    this.newGame()
+
+    const coll = collection(this.firestore, 'games');
+    this.games$ = collectionData(coll);
+    this.games$.subscribe( newGameInfos => {
+      console.log('Game update', newGameInfos);
+    })
+
+    // this
+      // .firestore.collection('games')
+      // .valueChanges()
+      // .subscribe((game: any) => {
+      //   console.log('Game update', game);
+      // })
+  }
+
+  async newGame() {
+    this.game = new Game();
+    console.log(this.game)
+    
+    const coll = collection(this.firestore, 'games');
+    let gameInformation = await addDoc(coll, {game: this.game.toJSON()} );
+    console.log(gameInformation);
+    
+  }
+
+  takeCard() {
+    if(!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      console.log(this.currentCard)
+      this.pickCardAnimation = true;
+      
+      console.log('New Card ',this.currentCard);
+      console.log('Game is ',this.game);
+      
+      //Nächster Spieler
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+
+      setTimeout(()=>{
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+        }, 1000);
+  }
+
+  
+  
+    
+  
+
+}
+
+
+
+}
